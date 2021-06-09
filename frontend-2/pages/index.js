a734c8e624f943b9d140a0903248764e61905ae5
Mateus José Barbosa <mateus@findme.id>
2021-06-08 22:57:52 -0300

    fix: retorno aos dados fixos

--- frontend-2/pages/index.js
+++ frontend-2/pages/index.js
@@ -11,10 +11,29 @@ export default function Home({ mapMarkers }) {
 }
 
 Home.getInitialProps = async () => {
-  const res = await fetch("http://localhost:3004/mapMarkers");
-  const json = await res.json();
-
   return {
-    mapMarkers: json,
+    mapMarkers: [
+      {
+        position: {
+          lat: -19.90375588214059,
+          lng: -43.93853426116382,
+        },
+        status: "flooded",
+      },
+      {
+        position: {
+          lat: -19.933599741640776,
+          lng: -43.98515652370336,
+        },
+        status: "floodWarning",
+      },
+      {
+        position: {
+          lat: -19.890178372113155,
+          lng: -43.92063718681564,
+        },
+        status: "floodWarning",
+      },
+    ],
   };
 };
