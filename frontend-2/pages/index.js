0a31259a29c5b0585e448e1bc633f5f3d4d89622
Mateus José Barbosa <mateus@findme.id>
2021-06-08 22:56:02 -0300

    feat: add initial props

--- frontend-2/pages/index.js
+++ frontend-2/pages/index.js
@@ -11,29 +11,10 @@ export default function Home({ mapMarkers }) {
 }
 
 Home.getInitialProps = async () => {
+  const res = await fetch("http://localhost:3004/mapMarkers");
+  const json = await res.json();
+
   return {
-    mapMarkers: [
-      {
-        position: {
-          lat: -19.90375588214059,
-          lng: -43.93853426116382,
-        },
-        status: "flooded",
-      },
-      {
-        position: {
-          lat: -19.933599741640776,
-          lng: -43.98515652370336,
-        },
-        status: "floodWarning",
-      },
-      {
-        position: {
-          lat: -19.890178372113155,
-          lng: -43.92063718681564,
-        },
-        status: "floodWarning",
-      },
-    ],
+    mapMarkers: json,
   };
 };
